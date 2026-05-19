e93680e9a97a55a9476d30b14c8f084e0dab13f3
ninarhop <nina@arcore.us>
2026-05-19 12:34:37 -0500

    Create data.js with legislators information
    Added initial data for legislators including names, chambers, districts, parties, and latest votes.

---
+++ frontend/data.js
@@ -0,0 +1,30 @@
+const legislators = [
+  {
+    name: "Brian S. Evans",
+    chamber: "House",
+    district: 68,
+    party: "Republican",
+    city: "",
+    county: "",
+    committees: [],
+    latestVotes: [
+      { bill: "HB1001", vote: "Yea" },
+      { bill: "SB1", vote: "Yea" }
+    ],
+    notes: []
+  },
+  {
+    name: "Andrew Collins",
+    chamber: "House",
+    district: 73,
+    party: "Democrat",
+    city: "",
+    county: "",
+    committees: [],
+    latestVotes: [
+      { bill: "HB1001", vote: "Nay" },
+      { bill: "SB1", vote: "Nay" }
+    ],
+    notes: []
+  }
+];
